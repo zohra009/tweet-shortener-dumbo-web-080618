0026260fdff07ec6834eb730bc24c876fc45304e
Deniz Unat <deniznida@users.noreply.github.com>
2015-10-20 12:30:11 -0400

    Update tweet_shortener.rb

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -51,7 +51,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-    word_substituter(tweet)[0..136] + ('...')
+    word_substituter(tweet)[0..136] + '...'
   else
     tweet
   end
