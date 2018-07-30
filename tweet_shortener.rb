d339928b5ebfe14b7af6cf404eca9efb8b2e2c77
Zohra A <anzohra@outlook.com>
2018-07-30 05:08:38 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,45 +3,57 @@ require "pry"
 
 def dictionary
   convert = {
-    "hello" => "hi",
-    "to" => "2",
-    "two" => "2",
-    "too" => "2",
-    "for" => "4",
-    "four" => "4",
-    "be" => "b",
-    "you" => "u",
-    "at" => "@",
-    "and" => "&",
-  }
-end 
-  def word_substituter(tweet)
-    # replaces long words with their expected short form
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+}
+end
 
-      tweet.split(" ").map do |word|
-        if dictionary.keys.include?(word.downcase)
-          word = dictionary[word.downcase]
-        else
-          word
-      end
-    end.join(" ")
+def word_substituter(tweet)
+  # replaces long words with their expected short form
+
+    tweet.split(" ").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  tweet.map do |phrase|
+    puts word_substituter(phrase)
   end
+end
 
-  def bulk_tweet_shortener(tweet)
-    tweet.map do |phrase|
-      puts word_substituter(phrase)
+def selective_tweet_shortener(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)
+    elsif phrase.length <= 140
+      phrase
     end
   end
+end
 
-  def selective_tweet_shortener(tweet)
-    tweet.split(" ").map do |phrase|
-      if phrase.length > 140
-        word_substituter(phrase)
-      elsif phrase.length <= 140
-        phrase
-      end
+def shortened_tweet_truncator(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)[0..140] + "..."
+    else
+      phrase
     end
-  end
+  end.join(" ")
+end
+
 
   def shortened_tweet_truncator(tweet)
     tweet.split(" ").map do |phrase|
